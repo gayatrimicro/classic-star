379f4803c712e71cd0823f6e6a49db84941c9b56
Rits-theDeveloper <ritesh.mestry.naresh@gmail.com>
2020-09-04 12:48:01 +0530

    classic star initial commit

---
+++ contact.php
@@ -0,0 +1,352 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Contact Us</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="assets/vendor/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+
+  <link href="assets/css/style.css" rel="stylesheet">
+
+
+  <style type="text/css">
+    .row{
+      margin: 0px !important;
+    }
+    .Banner_Site{
+      background-color: skyblue;
+    }
+    .inner_part{
+      padding: 0px 25px;
+    }
+    .inner_part h4{
+      margin-bottom: 7px;
+    }
+    .Small_label{
+      font-size: 13px;
+        font-weight: 600;
+        margin-bottom: 25px;
+        display: block;
+        text-transform: uppercase;
+    }
+    .inner_part p{
+      margin-bottom: 0px;
+        font-size: 17px;
+        line-height: 35px;
+    }
+    .inner_part a{
+      text-decoration: none;
+    }
+    .inner_contact{
+      margin-top: 35px;
+    }
+    .inner_contact h5{
+        margin-top: 35px;
+        font-size: 18px;
+        letter-spacing: 1px;
+    }
+    .Back_lft{
+      background-color: #333333;
+      color: #fff;
+    }
+    .Back_lft a{
+      color: #fff;
+    }
+    .Form_part{
+      padding: 70px 0px;
+    }
+    .Clr_lbl{
+      color: #0072bb;
+      margin-bottom: 15px;
+    }
+    .inner_part h3{
+        font-size: 38px;
+        font-weight: 700;
+        margin-bottom: 5px;
+    }
+    #form_contact{
+      margin-top: 20px;
+    }
+    #form_contact input{
+      border-radius: 0px;
+      height: 43px;
+    }
+    #form_contact label{
+      font-size: 14px;
+    }
+    #form_contact .form-group{
+      margin-bottom: 15px;
+    }
+    #form_contact label span{
+      display: inline-block;
+      margin-left: 5px;
+      color: red;
+      font-size: 14px;
+      font-weight: 600;
+    }
+    #form_contact textarea{
+      border-radius: 0px;
+    }
+    .Btn_send{
+      margin-top: 25px;
+        border: none;
+        background-color: #0072bb;
+        color: #fff;
+        outline: none;
+        padding: 12px 25px;
+        font-size: 17px;
+        letter-spacing: 1px;
+    }
+    .heigt{
+      padding: 40px 50px;
+    }
+    .inner_time{
+      margin-top: 35px;
+      margin-bottom: 35px;
+    }
+    .inner_part h5 i{
+      margin-left: 10px;
+    }
+  </style>
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container-fluid">
+
+      <div class="row justify-content-center">
+        <div class="col-xl-11 d-flex align-items-center">
+          <h1 class="logo mr-auto"><a href="index.html">
+            <img src="assets/img/classic-star.png">
+          </a></h1>
+          <!-- Uncomment below if you prefer to use an image logo -->
+          <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+          <nav class="nav-menu d-none d-lg-block">
+            <ul>
+              <li><a href="index.html">Home</a></li>
+              <li><a href="#about">About Us</a></li>
+              <li><a href="#services">Products</a></li>
+              <!-- <li><a href="#portfolio">Portfolio</a></li> -->
+              <!-- <li><a href="#team">Team</a></li> -->
+              <!-- <li class="drop-down"><a href="">Products</a>
+                <ul>
+                  <li><a href="#">Anchors</a></li>
+                  <li><a href="#">Bolts</a></li>
+                  <li><a href="#">Socket & Set Screws</a></li>
+                  <li><a href="#">Hook & Eye Bolts</a></li>
+                  <li><a href="#">Pins</a></li>
+                  <li><a href="#">Rivets</a></li>
+                  <li><a href="#">Screws</a></li>
+                  <li><a href="#">Studs</a></li>
+                  <li><a href="#">Self-Drilling Screws</a></li>
+                  <li><a href="#">Self-Tapping Screws</a></li>
+                  <li><a href="#">Quard Steel Plates</a></li>
+                  <li><a href="#">Nails </a></li>
+                  <li><a href="#">Nuts</a></li>
+                  <li><a href="#">Threaded Rods</a></li>
+                  <li><a href="#">Wall Plugs</a></li>
+                  <li><a href="#">Washers</a></li>
+                </ul>
+              </li> -->
+              <li class="active"><a href="#contact">Contact Us</a></li>
+
+            </ul>
+          </nav><!-- .nav-menu -->
+        </div>
+      </div>
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs Section ======= -->
+    <section class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Contact Us</h2>
+          <ol>
+            <li><a href="index.php">Home</a></li>
+            <li>Contact Us</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs Section -->
+
+    <section class="Banner_Site">
+    <div id="googleMap" style="width:100%;height:400px;"></div>
+  </section>
+  <section class="Form_part">
+    <div class="container">
+      <div class="row">       
+        <div class="col-sm-6">
+          <div class="inner_part">
+            <span class="Small_label Clr_lbl">CONTACT</span>
+            <h3>Have a question?</h3>
+            <p>Please get in touch with us and we will we happy to help you</p>
+            <form id="form_contact">
+              <div class="form-group">
+                <label for="">Your Name<span>*</span></label>
+                  <input type="text" class="form-control" id="" placeholder="" name="">
+              </div>
+              <div class="form-group">
+                <label for="">Your Email<span>*</span></label>
+                  <input type="text" class="form-control" id="" placeholder="" name="">
+              </div>
+              <div class="form-group">
+                <label for="">Phone No.<span>*</span></label>
+                  <input type="text" class="form-control" id="" placeholder="" name="">
+              </div>
+              <div class="form-group">
+                <label for="">Subject<span>*</span></label>
+                  <input type="text" class="form-control" id="" placeholder="" name="">
+              </div>
+              <div class="form-group">
+                <label for="">Your Message<span>*</span></label>
+                  <textarea id="" name="" rows="5" class="form-control"></textarea>
+              </div>
+              <button type="submit" class="Btn_send">Submit</button>
+            </form>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="inner_part Back_lft heigt">
+            <h4>Location</h4>
+            <span class="Small_label">Classic Star Building Materials LLC</span>
+            <p>Shop No. 06, Waqf building</p>
+            <p>Behind Madina supermarket</p>
+            <p>Naif area, Deira</p>
+            <p>Dubai, United Arab Emirates</p>
+
+            <div class="inner_contact">
+              <p><b>Phone : </b> <a href="tel:+971 4 330 3352">+971 4 330 3352</a></p>
+              <p><b>WhatsApp Chat No : </b> <a href="tel:+971 50 634 9042">+971 50 634 9042</a></p>
+              <p><b>Fax : </b> +971 4 296 6383</p>
+              <p><b>Email : </b> <a href="mailto:sales@clmet.com">sales@clmet.com</a> / <a href="mailto:info@clmet.com">info@clmet.com</a></p>
+            </div>
+            <div class="inner_time">
+              <p><b>Store Timings :</b></p>
+              <p>Saturday – Thursday</p>
+              <p>8:00am – 1:00pm</p>
+              <p>4:00pm – 8:00pm</p>
+              <p>Closed on Fridays</p>            
+            </div>
+            <h5><a href="https://maps.google.com/maps?q=25.269756317138672%2C55.30437088012695&z=17&hl=en" target="_blank">GET DIRECTIONS <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span></a></h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </section>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-info">
+            <!-- <img src="assets/img/classic-star.png" style="width:100%;"> -->
+            <!-- <h3>Classic Star Building Materials LLC</h3> -->
+            <p>Classic Star Building Materials LLC, is your trusted partner for all your fasteners requirements. Located in Deira Dubai hardware & tools market, Classic Star is known for its quality and availability of extensive range of Stainless-Steel fasteners. We carry 100% authentic grade materials such as 304, 316, and other carbon and alloy steel fasteners.</p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="ion-ios-arrow-right"></i> <a href="#">Home</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="#">About us</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="#">Products</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="#">Contact us</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Contact Us</h4>
+            <p>
+              Shop No. 06, Waqf building,<br> 
+              Behind Madina supermarket,<br>
+              Naif area, Deira,<br> 
+              Dubai, United Arab Emirates,<br>
+              <strong>Phone:</strong> +971 4 330 3352<br>
+              <strong>Fax:</strong> +971 4 296 6383<br>
+              <!-- <strong>Email:</strong> info@example.com<br> -->
+            </p>
+
+            <div class="social-links">
+              <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+              <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+              <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+              <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+            </div>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-newsletter">
+            <h4>Our Newsletter</h4>
+            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam illum dolore legam minim quorum culpa amet magna export quem marada parida nodela caramase seza.</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>Classic Star Building Materials LLC</strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+       
+        Designed by <a href="https://ibridgedigital.com/">iBridge Digital</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <!-- Uncomment below i you want to use a preloader -->
+  <!-- <div id="preloader"></div> -->
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
